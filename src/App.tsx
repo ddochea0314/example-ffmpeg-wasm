3ed3806905d6ccdc133dddfb2f5f99a8fede4df3
ddochea0314 <ddochea0314@gmail.com>
2021-05-05 12:06:39 +0900

    add react-router-dom and split example

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import "./App.css";
+
+import AviToMp4 from "./views/AviToMp4";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <BrowserRouter>
+  <Link to="/">Example 1 (AviToMp4)</Link>
+  <br />
+  <Switch>
+    <Route path="/">
+      <AviToMp4 />
+    </Route>
+  </Switch>
+  </BrowserRouter>
   );
 }
 
---
+++ src/views/AviToMp4.tsx
@@ -0,0 +1,35 @@
+import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
+import { useState } from "react";
+
+function AviToMp4(): JSX.Element {
+  const [videoSrc, setVideoSrc] = useState("");
+  const [message, setMessage] = useState("Click Start to transcode");
+  const ffmpeg = createFFmpeg({
+    log: true,
+  });
+
+  const doTranscode = async () => {
+    setMessage("Loading ffmpeg-core.js");
+    await ffmpeg.load();
+    setMessage("Start transcoding");
+    ffmpeg.FS("writeFile", "test.avi", await fetchFile("/flame.avi"));
+    await ffmpeg.run("-i", "test.avi", "test.mp4");
+    setMessage("Complete transcoding");
+    const data = ffmpeg.FS("readFile", "test.mp4");
+    setVideoSrc(
+      URL.createObjectURL(new Blob([data.buffer], { type: "video/mp4" }))
+    );
+  };
+
+  return (
+    <div className="App">
+      <p />
+      <video src={videoSrc} controls></video>
+      <br />
+      <button onClick={doTranscode}>Start</button>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default AviToMp4;
