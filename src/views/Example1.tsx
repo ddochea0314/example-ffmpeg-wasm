88708451686fe40971fd62221222b54a5902dba1
ddochea0314 <ddochea0314@gmail.com>
2021-05-08 15:32:41 +0900

    메모리 누수 완화

--- src/views/Example1.tsx
+++ src/views/Example1.tsx
@@ -1,24 +1,26 @@
 // original source. https://github.com/ffmpegwasm/react-app
 // change typescript(.tsx)
 
-import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
-import { useState } from "react";
+import FFmpegContext from "../context/FFmpegContext";
+import { fetchFile, FFmpeg } from "@ffmpeg/ffmpeg";
+import { useState, useContext } from "react";
 
 function AviToMp4(): JSX.Element {
   const [videoSrc, setVideoSrc] = useState("");
   const [message, setMessage] = useState("Click Start to transcode");
-  const ffmpeg = createFFmpeg({
-    log: true,
-  });
-
+  const ffmpeg = useContext<FFmpeg>(FFmpegContext);
+  
   const doTranscode = async () => {
     setMessage("Loading ffmpeg-core.js");
-    await ffmpeg.load();
+    if(!ffmpeg.isLoaded()){
+      await ffmpeg.load();
+    }
     setMessage("Start transcoding");
     ffmpeg.FS("writeFile", "test.avi", await fetchFile("/flame.avi"));
     await ffmpeg.run("-i", "test.avi", "test.mp4");
     setMessage("Complete transcoding");
     const data = ffmpeg.FS("readFile", "test.mp4");
+    URL.revokeObjectURL(videoSrc);
     setVideoSrc(
       URL.createObjectURL(new Blob([data.buffer], { type: "video/mp4" }))
     );
